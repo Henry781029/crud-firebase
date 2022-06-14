6226508fc9de11d85eeeba9ee6bf760f66e7da9d
Henry781029 <hcramosp@outlook.com>
2022-06-14 11:37:53 -0500

    se crea input de nombre y precio

--- src/screens/Agregar.js
+++ src/screens/Agregar.js
@@ -5,7 +5,7 @@ import EmojiPicker from "rn-emoji-keyboard";
 
 export default function Agregar(){
 
-    const [isOpen, setIsOpen ] = react.useState(true);
+    const [isOpen, setIsOpen ] = react.useState(false);
     const [NuevoItem, setNuevoItem] = react.useState({
         img: '🐎',
         nombre: ' ',
@@ -23,17 +23,29 @@ export default function Agregar(){
 
     return(
         <RN.View style = {estilo.vista}>
-            <RN.Text>
-            Vender o Nuevo Producto
+            <RN.Text style={estilo.titulo}>
+                Nuevo Producto
             </RN.Text>
-            <RN.Text>
-            {NuevoItem.img}
+            <RN.Text style={estilo.emoji} onPress={()=>setIsOpen(true)}>
+                {NuevoItem.img}
+            </RN.Text>
             <EmojiPicker
-            onEmojiSelected={handlePick}
-            open={isOpen}
-            onClose={()=>setIsOpen(false)}
+                onEmojiSelected={handlePick}
+                open={isOpen}
+                onClose={()=>setIsOpen(false)}
             />
-            </RN.Text>
+            <RN.TextInput
+                onChangeText={(text)=>setNuevoItem({...NuevoItem, nombre:text})}
+                placeholder="Nombre del Producto"
+                style = {estilo.container}
+            />
+            <RN.TextInput
+                onChangeText={(text)=>setNuevoItem({...NuevoItem, precio:text})}
+                placeholder="$ precio"
+                style = {estilo.container}
+                keyboardType="number-pad"
+            />
+            {/*<RN.Button title="Ingresar bd" onPress={(onSend)} />*/}
         </RN.View>    
     );
 };
@@ -43,5 +55,25 @@ const estilo = RN.StyleSheet.create({
         flex: 1,
         backgroundColor: "white",
         alignItems:"center"
+    },
+    titulo:{
+        fontSize: 32,
+        fontWeight: '700',
+    },
+    emoji:{
+        fontSize: 100,
+        borderWidth: 1,
+        borderColor: "red",
+        borderRadius: 6,
+        padding: 10,
+        marginVertical: 6
+    },
+    container:{
+        width: '90%',
+        padding: 13,
+        marginVertical: 6,
+        borderWidth: 1,
+        borderColor:"red",
+        borderRadius: 6
     }
 })
